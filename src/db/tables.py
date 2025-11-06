80aa8d89ee1885c4b02e0f75c7156b79234d05e7
Grigoriy Arzhevitin <arzhevitin.g@gmail.com>
2025-11-06 10:59:37 +0500

    add tables models

---
+++ src/db/tables.py
@@ -0,0 +1,59 @@
+from datetime import datetime, date
+from sqlalchemy import Boolean, Date, DateTime, ForeignKey, Integer, MetaData, String, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Table(DeclarativeBase):
+    convention = {
+        "all_column_names": lambda constraint, _: "_".join([
+            column.name for column in constraint.columns.values()
+        ]),
+        "ix": "ix__%(table_name)s__%(all_column_names)s",
+        "uq": "uq__%(table_name)s__%(all_column_names)s",
+        "ck": "ck__%(table_name)s__%(constraint_name)s",
+        "fk": "fk__%(table_name)s__%(all_column_names)s__%(referred_table_name)s",
+        "pk": "pk__%(table_name)s",
+    }
+
+    metadata = MetaData()
+    metadata.naming_convention = convention
+
+
+class BatteryTable(Table):
+    __tablename__ = "batteries"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    voltage: Mapped[int] = mapped_column(Integer, nullable=False)
+    capacity: Mapped[int] = mapped_column(Integer, nullable=False)
+    validity_period: Mapped[date] = mapped_column(Date)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now()
+    )
+
+    device_id: Mapped[int] = mapped_column(ForeignKey("devices.id", ondelete="CASCADE"), nullable=False)
+    device: Mapped["DeviceTable"] = relationship(
+        back_populates="batteries"
+    )
+
+
+class DeviceTable(Table):
+    __tablename__ = "devices"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    version: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now()
+    )
+
+    batteries: Mapped[list["BatteryTable"]] = relationship(
+        back_populates="device",
+    )
